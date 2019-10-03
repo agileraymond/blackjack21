4a55250c0c5cfdbebfc72a210b06fac97dbd8b08
Christoffer Aske <exouxas@gmail.com>
2019-10-03 20:42:58 +0200

    Added a shuffle method for the Deck class and propertly implemented the constructor

--- src/Deck.cs
+++ src/Deck.cs
@@ -1,12 +1,37 @@
 using System.Collections.Generic;
+using System;
 
 namespace blackjack21
 {
     public class Deck : List<Card>
     {
+        Random random = new Random();
+
         public Deck()
         {
-            this.Add(new Card(CardSuit.Diamonds, CardRank.Ace));
+            foreach(CardSuit suit in Enum.GetValues(typeof(CardSuit)))
+            {
+                foreach(CardRank rank in Enum.GetValues(typeof(CardRank)))
+                {
+                    this.Add(new Card(suit, rank));
+                }
+            }
+        }
+
+        public void Shuffle(this Deck deck)
+        {
+            List<Card> mirror = deck;
+            deck = new List<Card>();
+            int n;
+
+
+            for(int i = 0; i < mirror.Count; i++)
+            {
+                n = random.Next(0, mirror.Count);
+
+                deck.Add(mirror[n]);
+                mirror.RemoveAt(n);
+            }
         }
     }
 }
